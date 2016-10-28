b84d890d11bc5a0cabc4171a5d926f7534637176
tianbo <tb@bimt.com>
2016-10-28 17:54:40 +0800

    add osx

---
+++ CommonCryptoOSX/CommonCryptoOSX.h
@@ -0,0 +1,19 @@
+//
+//  CommonCryptoOSX.h
+//  CommonCryptoOSX
+//
+//  Created by bimt-bob on 2016/10/28.
+//  Copyright © 2016年 bimt-jlf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CommonCryptoOSX.
+FOUNDATION_EXPORT double CommonCryptoOSXVersionNumber;
+
+//! Project version string for CommonCryptoOSX.
+FOUNDATION_EXPORT const unsigned char CommonCryptoOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCryptoOSX/PublicHeader.h>
+
+
